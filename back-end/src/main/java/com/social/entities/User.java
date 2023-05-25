ef45961d5e7ad948e14e0c53cdea386a5157b7da
iruora <arrouurii@gmail.com>
2023-05-25 16:19:36 +0200

    chore: upgrade to spring boot 2

--- back-end/src/main/java/com/social/entities/User.java
+++ back-end/src/main/java/com/social/entities/User.java
@@ -19,129 +19,131 @@ import org.springframework.security.core.userdetails.UserDetails;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 /**
  * Description of User.
- * 
+ *
  * @author kamal berriga
  */
 @Entity
-@Table(name="User")
+@Table(name = "User")
 @Scope("session")
-public  class User implements UserDetails{
-	public static enum Role{ USER }
-	/**
-	 * Description of the property id.
-	 */
-	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private Long id ;
-	/**
-	 * Description of the property email.
-	 */
-	@Column(unique = true)
-	private String username ;
-	/**
-	 * Description of the property password.
-	 */
-	@JsonProperty(access = Access.WRITE_ONLY)
-	private String password ;
-	/**
-	 * Description of the property role , to grant authority to the user .
-	 */
-    private String  role;
+public class User implements UserDetails {
+    public static enum Role {USER}
+
     /**
-	 * Description of the property full name.
-	 */
+     * Description of the property id.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /**
+     * Description of the property email.
+     */
+    @Column(unique = true)
+    private String username;
+    /**
+     * Description of the property password.
+     */
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private String password;
+    /**
+     * Description of the property role , to grant authority to the user .
+     */
+    private String role;
+    /**
+     * Description of the property full name.
+     */
     private String fullName;
 
-    public User(){
-    	
+    public User() {
+
     }
-    
-    public User(String username,String password,String fullName){
-    	this.username=username;
-    	this.password= password;
-    	this.fullName=fullName;
+
+    public User(String username, String password, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
     }
-	@JsonIgnore
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
 
-	@JsonIgnore
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @JsonIgnore
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
-	@JsonIgnore
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	@JsonIgnore
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@JsonIgnore
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Collection<GrantedAuthority> authorities = new ArrayList<>();
-		authorities.add(new SimpleGrantedAuthority(role));
-		return authorities;
-	}
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role +
-				 ",]";
-	}
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role));
+        return authorities;
+    }
 
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return password;
-	}
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role +
+                ",]";
+    }
 
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return username;
-	}
-	
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return password;
+    }
 
-	public String getRole() {
-		return role;
-	}
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return username;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getRole() {
+        return role;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-	public String getFullName() {
-		return fullName;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public Long getId() {
-		return id;
-	}
-	
-	
-	
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
 }
--- back-end/src/main/java/com/social/services/UserService.java
+++ back-end/src/main/java/com/social/services/UserService.java
@@ -5,29 +5,29 @@ import org.springframework.stereotype.Service;
 
 import com.social.dao.UserRepository;
 import com.social.entities.User;
-/** 
+
+/**
  * @author kamal berriga
- *
  */
 @Service
 public class UserService {
 
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-	public User save(User user) {
-		return userRepository.saveAndFlush(user);
-	}
+    public User save(User user) {
+        return userRepository.saveAndFlush(user);
+    }
 
-	public User update(User user) {
-		return userRepository.save(user);
-	}
+    public User update(User user) {
+        return userRepository.save(user);
+    }
 
-	public User find(String userName) {
-		return userRepository.findOneByUsername(userName);
-	}
+    public User find(String userName) {
+        return userRepository.findOneByUsername(userName);
+    }
 
-	public User find(Long id) {
-		return userRepository.findOne(id);
-	}
+    public User find(Long id) {
+        return userRepository.getOne(id);
+    }
 }
